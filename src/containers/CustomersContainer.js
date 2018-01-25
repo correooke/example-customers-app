9b40294632f42a45e40088f51fc3ba7e9b5cf40b
HSNBDES33\eocariz <eocariz@hasar.com>
2018-01-24 21:14:03 -0300

    197 insert con reducer

--- src/containers/CustomersContainer.js
+++ src/containers/CustomersContainer.js
@@ -11,7 +11,9 @@ import { getCustomers } from './../selectors/customers';
 class CustomersContainer extends Component {
 
     componentDidMount() {
-        this.props.fetchCustomers();
+        if (this.props.customers.length === 0) {
+            this.props.fetchCustomers();
+        }
     }
     
     handleAddNew = () => {
--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -1,7 +1,8 @@
 import { handleActions } from 'redux-actions';
-import { FETCH_CUSTOMERS } from './../constants/index';
+import { FETCH_CUSTOMERS, INSERT_CUSTOMER } from './../constants/index';
 
 export const customers = handleActions({
     [FETCH_CUSTOMERS]: (state, action) => [ ...action.payload],
+    [INSERT_CUSTOMER]: (state, action) => [ ...state, action.payload ],
 }, []);
 
