fb3fea9fc8e8b6e2731216d9e9b60b5cac23f570
HSNBDES33\eocariz <eocariz@hasar.com>
2018-01-24 21:41:05 -0300

    v198 update y método ES6 Reduce

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -1,8 +1,38 @@
 import { handleActions } from 'redux-actions';
-import { FETCH_CUSTOMERS, INSERT_CUSTOMER } from './../constants/index';
+import { FETCH_CUSTOMERS, INSERT_CUSTOMER, UPDATE_CUSTOMER } from './../constants/index';
 
 export const customers = handleActions({
     [FETCH_CUSTOMERS]: (state, action) => [ ...action.payload],
     [INSERT_CUSTOMER]: (state, action) => [ ...state, action.payload ],
+    [UPDATE_CUSTOMER]: (state, action) => {
+        const customerPayload = action.payload;
+        const { id } = customerPayload; // i = 2 name='nuevo nombre'
+        // [ { id: 1, name: '', ... }, 
+        //   { id: 2, name: 'viejo nombre', ... },
+        //   { id: 3, name: '', ... }]
+        const customers = state;
+        const initialValue = [];
+        // primer iteracion
+        // acc = []
+        // { id: 1, name: '', ... }
+        // [ { id: 1, name: '', ... } ] 
+
+        // segunda iteración 
+        // acc = [ { id: 1, name: '', ... } ] 
+        // { id: 2, name: 'viejo nombre', ... } => { id: 2, name: 'nuevo nombre', ... }
+        // [ { id: 1, name: '', ... }, { id: 2, name: 'nuevo nombre', ... } ]
+        
+        // tercera iteración 
+        // acc = [ { id: 1, name: '', ... }, { id: 2, name: 'nuevo nombre', ... } ]
+        const newCustomers = customers.reduce( (acc, customer) => {
+            if (customer.id === id) {
+                return [ ...acc, customerPayload];
+            } else {
+                return [ ...acc, customer ];
+            }
+        }, initialValue);
+
+        return newCustomers;
+    }
 }, []);
 
